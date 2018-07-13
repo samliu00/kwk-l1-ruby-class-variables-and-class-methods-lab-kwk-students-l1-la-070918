26c47c42272e926069c9bd59a02ef5c6473e63a0
samliu00 <samanthaliu19@gmail.com>
2018-07-13 18:55:47 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,52 @@
 
+class Song 
+
+attr_accessor :name, :artist, :genre
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, genres, artists)
+    @name = Song.name
+    @@count += 1 
+    @@genres << Song.genres
+    @@artists << Song.artists
+  end 
+  
+  def Song.name
+    return @name 
+  end
+  
+  def Song.count
+    return @@count 
+  end 
+  
+  def Song.genres 
+    return @@genres
+  end 
+  
+  def Song.artists
+    return @@artists
+  end 
+
+  def Song.genre_count
+    items = {}
+    new_item = "#{Song.genres}"
+    item_count = "#{Song.count}"
+    items[new_item] = item_count
+  end 
+  
+  Song.genre_count
+  
+  def Song.artist_count
+    items_artist = {}
+    new_item_artist = "#{Song.artists}"
+    artist_count = "#{Song.count}"
+    items_artist[new_item_artist] = artist_count
+  end 
+  
+  Song.artist_count
+  
+  
+end 
+
